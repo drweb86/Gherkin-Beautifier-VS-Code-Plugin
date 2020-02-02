22537c89946712713b6db439ee60362be5d3c299
Siarhei Kuchuk <cuchuk.sergey@gmail.com>
2020-02-02 16:35:24 +0300

    cleanup

--- extension.js
+++ extension.js
@@ -1,26 +1,17 @@
 'use strict';
 
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
 const identSymbol = vscode.workspace.getConfiguration().get('conf.view.indentSymbol') === 'tab' ? '\t' : ' ';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  // console.log('Congratulations, your extension "4to2" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
   context.subscriptions.push(vscode.commands.registerTextEditorCommand('Gherkin-Beautifier-VS-Code-Plugin.beautify', function (textEditor, edit) {
-    // The code you place here will be executed every time your command is executed
 
-    // Display a message box to the user
-    // vscode.window.showInformationMessage('Hello World!');
+    const fileName = vscode.window.activeTextEditor.document.uri.fsPath;
+    if (!fileName.endsWith('.feature')) {
+      return;
+    }
+
     const document = textEditor.document;
     for (let lineNumber = 0, lineCount = document.lineCount; lineNumber < lineCount; lineNumber++) {
       const
@@ -36,7 +27,6 @@ function activate(context) {
 
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {
 }
 
